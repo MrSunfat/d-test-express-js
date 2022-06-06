6e34a243d5d75ea5d9cda424002971e17fa787e9
MrSunfat <trannhatdanh15062001@gmail.com>
2022-06-06 10:01:28 +0700

    U

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use('/public', express.static(path.join(__dirname, "public")));
 
@@ -9,6 +10,6 @@ app.get('/', (req, res) => {
     res.sendFile(duongDan);
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port http://localhost:${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
